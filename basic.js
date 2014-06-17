b6adcab9b7a7460a5de9c0d2d4bd07ec8aaeabdc
Sam <knitz4sanity@gmail.com>
2014-06-17 17:44:25 -0600

    testing only

---
+++ basic.js
@@ -0,0 +1,16 @@
+
+
+v("hi")
+
+function v(val){
+    console.log(val)
+}
+
+console.log("hi");
+
+var test_array = new Array();
+for (i = 0; i < 10; i++) {
+    test_array[i] = i;
+}
+x = test_array[4];
+console.log(x);
