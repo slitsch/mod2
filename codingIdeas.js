9d99c8c9015554e530e32f76f9658a9ae898eca9
Sam <knitz4sanity@gmail.com>
2014-07-04 20:20:23 -0600

    More string functions implemented from nw list in basecamp

--- codingIdeas.js
+++ codingIdeas.js
@@ -7,10 +7,24 @@
 /*String stuff
 * write a fn that escapes html entities like "&" -> amp*/
 
-var inputText = "Hi! & Bye #1";
+var inputText = "Hi! & \nBye #1\n";
+console.log("inputText: ", inputText);
 
 var result = escapeInputText(inputText);
-console.log(result)
+console.log("escapeInputText ", result);
+
+var slugResult = slugize(inputText);
+console.log("slugResult: ", slugResult);
+
+var nl2brResult = newline2br(inputText);
+console.log("newline2br: ", nl2brResult);
+
+var badNum = "12309gy65784";
+var badNum2 = "1234567891011"
+var goodNum = "5309259837";
+console.log("phoneFormat bad: ",phoneFormat(badNum));
+console.log("phoneFormat bad: ",phoneFormat(badNum2));
+console.log("phoneFormat good: ",phoneFormat(goodNum));
 
 function escapeInputText(str){
     var arr = str.split('');
@@ -42,6 +56,44 @@ function escapeInputText(str){
     return escaped.join('')
 }
 
-function unEscapeText(str){
+function slugize(str) {
+//    slugize a string. replace all none alphanumeric characters with a "-"
+// so "hi there" turns to "hi-there" (url friendly)
+  return str.replace(/(\W)/g,"-");
+}
+
+function newline2br(str) {
+//    newline to br - replace text newline breaks with the html equivalent <br />
+    return str.replace("\n", "<br>")
+}
+
+
+function phoneFormat(numStr) {
+//    phone number format - detect if a string is a phone number
+// and properly format it with dashes.
+    var reg = /\D +/; //includes non-number chars
+    if (numStr.match(reg)) {
+        return "Non-numbers are included: string invalid."
+    }
+   if (numStr.length > 10) {
+    if (numStr[0] ==="0") {
+        return "String is long, it might be an international number, I'll ignore it";
+    }
+    else{
+        return "String is too long and contains characters; it is not a phone number.";
+    }
+   } else {
+       var newNum = parseInt(numStr,10);
+//       divide to get areaCode, digit3, digit4 or slice
+//       var areaCode = parseInt(newNum/ 10000000);
+       var areaCode = numStr.slice(0,3);
+       var digit3 = numStr.slice(3,6);
+       var digit4 = numStr.slice(6,10);
+       return areaCode + "-" + digit3 + "-" + digit4;
+//       console.log(areaCode, digit3, digit4)
+   }
+
+
+
 
 }
